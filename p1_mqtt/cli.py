a70914dbfb4ee469ced234f427e57946355d205c
Ralf Ertzinger <ralf@skytale.net>
2021-01-09 16:57:05 +0000

    Add CLI

---
+++ p1_mqtt/cli.py
@@ -0,0 +1,229 @@
+"""
+This file contains the CLI script entry points
+"""
+
+import argparse
+import codecs
+import configparser
+import logging
+import multiprocessing
+import time
+from typing import Any, Dict, List
+
+from .mqtt import mqtt_main
+from .p1serial import p1serial_main
+
+logging.basicConfig(
+    format="%(asctime)-15s %(levelname)s: %(message)s", level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
+
+# Default values for command line args
+DEFAULTS: Dict[str, Any] = {
+    "mqtt_port": 1883,
+    "buffer_size": 100000,
+    "mqtt_topic": "p1-mqtt/tele/%(channel)s/%(serial)s/SENSOR",
+    "mqtt_client_id": "p1-mqtt-gateway",
+}
+
+
+def load_config_file(filename: str) -> Dict[str, Any]:
+    """
+    Load the ini style config file given by `filename`
+    """
+
+    config: Dict[str, Any] = {}
+    ini = configparser.ConfigParser()
+    try:
+        with codecs.open(filename, encoding="utf-8") as configfile:
+            ini.read_file(configfile)
+    except Exception as exc:
+        LOGGER.error("Could not read config file %s: %s", filename, exc)
+        raise SystemExit(1)
+
+    if ini.has_option("general", "device"):
+        config["device"] = ini.get("general", "device")
+
+    if ini.has_option("general", "mqtt-host"):
+        config["mqtt_host"] = ini.get("general", "mqtt-host")
+
+    try:
+        if ini.has_option("general", "mqtt-port"):
+            config["mqtt_port"] = ini.getint("general", "mqtt-port")
+    except ValueError:
+        LOGGER.error(
+            "%s: %s is not a valid value for mqtt-port",
+            filename,
+            ini.get("general", "mqtt-port"),
+        )
+        raise SystemExit(1)
+
+    if ini.has_option("general", "mqtt-client-id"):
+        config["mqtt_client_id"] = ini.get("general", "mqtt-client-id")
+
+    try:
+        if ini.has_option("general", "dewpoint"):
+            config["dewpoint"] = ini.getboolean("general", "dewpoint")
+    except ValueError:
+        LOGGER.error(
+            "%s: %s is not a valid value for dewpoint",
+            filename,
+            ini.get("general", "dewpoint"),
+        )
+        raise SystemExit(1)
+
+    try:
+        if ini.has_option("general", "buffer-size"):
+            config["buffer_size"] = ini.getint("general", "buffer-size")
+    except ValueError:
+        LOGGER.error(
+            "%s: %s is not a valid value for buffer-size",
+            filename,
+            ini.get("general", "buffer-size"),
+        )
+        raise SystemExit(1)
+
+    return config
+
+
+def p1_mqtt() -> None:
+    """
+    Main function for the p1-mqtt script
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", "-d", type=str, help="Serial device to use")
+    parser.add_argument("--config", type=str, help="Configuration file to load")
+    parser.add_argument(
+        "--mqtt-topic",
+        type=str,
+        default=None,
+        help="MQTT topic to publish to. May contain python format string "
+        "references to variables `serial` (containing the serial number "
+        "of the device generating the data) and `channel` (containing the "
+        "channel of the device generating the data). "
+        + ("(Default: %(mqtt_topic)s)" % DEFAULTS).replace("%", "%%"),
+    )
+    parser.add_argument("--mqtt-host", type=str, help="MQTT server to connect to")
+    parser.add_argument(
+        "--mqtt-port", type=int, default=None, help="MQTT port to connect to"
+    )
+    parser.add_argument(
+        "--mqtt-client-id",
+        type=str,
+        default=None,
+        help="MQTT client ID. Needs to be unique between all clients connecting "
+        "to the same broker",
+    )
+    parser.add_argument(
+        "--dsmr-22",
+        action="store_true",
+        help="Use DSMR 2.2 configuration for the serial port. The default is "
+        "to use DSMR 4.0 and newer.",
+    )
+    parser.add_argument(
+        "--serial-dump",
+        type=str,
+        default=None,
+        help="File name to dump all data read from the serial device to. "
+        "This is mainly for debugging purposes.",
+    )
+    parser.add_argument(
+        "--buffer-size",
+        type=int,
+        default=None,
+        help="How many measurements to buffer if the MQTT "
+        "server should be unavailable. This buffer is not "
+        "persistent across program restarts.",
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if args.config:
+        config = load_config_file(args.config)
+    else:
+        config = {}
+
+    LOGGER.debug("Config after loading config file: %s", config)
+
+    if args.device:
+        config["device"] = args.device
+
+    if args.mqtt_topic:
+        config["mqtt_topic"] = args.mqtt_topic
+    elif "mqtt_topic" not in config:
+        # Not set through config file, not set through CLI, use default
+        config["mqtt_topic"] = DEFAULTS["mqtt_topic"]
+
+    if args.mqtt_host:
+        config["mqtt_host"] = args.mqtt_host
+
+    if args.mqtt_port:
+        config["mqtt_port"] = args.mqtt_port
+    elif "mqtt_port" not in config:
+        # Not set through config file, not set through CLI, use default
+        config["mqtt_port"] = DEFAULTS["mqtt_port"]
+
+    if args.mqtt_client_id:
+        config["mqtt_client_id"] = args.mqtt_client_id
+    elif "mqtt_client_id" not in config:
+        # Not set through config file, not set through CLI, use default
+        config["mqtt_client_id"] = DEFAULTS["mqtt_client_id"]
+
+    if args.buffer_size:
+        config["buffer_size"] = args.buffer_size
+    elif "buffer_size" not in config:
+        # Not set through config file, not set through CLI, use default
+        config["buffer_size"] = DEFAULTS["buffer_size"]
+
+    if args.dsmr_22:
+        config["dsmr"] = "2.2"
+    else:
+        config["dsmr"] = "4.0"
+
+    if args.serial_dump:
+        config["serial_dump"] = args.serial_dump
+
+    LOGGER.debug("Completed config: %s", config)
+
+    if "device" not in config:
+        LOGGER.error("No serial device given")
+        raise SystemExit(1)
+
+    if "mqtt_host" not in config:
+        LOGGER.error("No MQTT host given")
+        raise SystemExit(1)
+
+    p1_mqtt_queue: multiprocessing.Queue = multiprocessing.Queue(
+        maxsize=config["buffer_size"]
+    )
+
+    procs: List[multiprocessing.Process] = []
+    p1_proc = multiprocessing.Process(
+        target=p1serial_main, name="p1", args=(p1_mqtt_queue, config)
+    )
+    p1_proc.start()
+    procs.append(p1_proc)
+
+    mqtt_proc = multiprocessing.Process(
+        target=mqtt_main, name="mqtt", args=(p1_mqtt_queue, config)
+    )
+    mqtt_proc.start()
+    procs.append(mqtt_proc)
+
+    # Wait forever for one of the processes to die. If that happens,
+    # kill the whole program.
+    while True:
+        for proc in procs:
+            if not proc.is_alive():
+                LOGGER.error("Child process died, terminating program")
+                break
+
+        time.sleep(1)
+
+    for proc in procs:
+        proc.terminate()
+    raise SystemExit(1)
---
+++ p1_mqtt/p1serial.py
@@ -0,0 +1,148 @@
+"""
+P1 to MQTT gateway. Handle the serial port reading
+and P1 telegram parsing
+"""
+
+import logging
+import multiprocessing
+from typing import Any, Dict
+
+import serial  # type: ignore
+
+from p1_mqtt.p1.parser import P1Parser
+
+LOGGER = logging.getLogger(__name__)
+
+# Serial port parameters for different DSMR
+# protocols
+DSMR_PARAMETERS = {
+    "2.2": {
+        "speed": 9600,
+        "databits": serial.SEVENBITS,
+        "stopbits": serial.STOPBITS_ONE,
+        "parity": serial.PARITY_EVEN,
+    },
+    "4.0": {
+        "speed": 115200,
+        "databits": serial.EIGHTBITS,
+        "stopbits": serial.STOPBITS_ONE,
+        "parity": serial.PARITY_NONE,
+    },
+}
+
+
+def p1serial_main(queue: multiprocessing.Queue, config: Dict[str, Any]) -> None:
+    """
+    Main function for the P1 process.
+
+    This will open the serial port, read data from it, feed it
+    to the P1 parser, take the parsed P1 telegrams, and send a
+    MQTT compatible serialized version to the queue
+    """
+
+    # Try to find out how many bytes to read from the serial port.
+    #
+    # Running the P1 parser is somewhat expensive, so ideally we want to
+    # run one complete telegram from the serial port (and not more!) and then
+    # run the parser to produce one telegram object, leaving no data in the
+    # parser buffer. We do not know the length of a telegram, though.
+    #
+    # The length of a telegram is pretty static, though. Most fields have
+    # a fixed length, and the fields that have a varying length change
+    # rarely.
+    #
+    # Hence we assume that the next telegram to read will have the same
+    # length as the last one parsed.
+    #
+    # If this assumption is wrong, one of two things will happen:
+    #
+    # - The new telegram is shorter than the old one. The parser will
+    #   produce a telegram, and have data remaining in it, which constitures
+    #   the start of another telegram. In this case, adjust the read size
+    #   to read the remainder of the next telegram. Once we are in sync
+    #   again (the parser produced a telegram and has 0 bytes remaining
+    #   in the buffer), readjust the read size to the new telegram size
+    #
+    # - The new telegram is longer than the old one. The parser will not
+    #   produce a telegram, and have data remaining in it. In this case,
+    #   adjust the read size down to the minimum, and read data until we
+    #   hit the first case, which will get us back into sync
+
+    # This is the size of the last telegram parsed, and our best guess
+    # as to the size of the next one.
+    telegram_size = 0
+
+    # This is the number of bytes to read from the serial port. As long as
+    # the size of a telegram does not change, this will be identical to
+    # telegram_size, but might change during telegram size changes.
+    #
+    # In any case, we'll never read less than 64 bytes to make sure we
+    # make forward progress.
+    #
+    # Start with 1k bytes.
+    serial_read_size = 1024
+
+    # Whether we consider ourselves to be in sync (one read from the
+    # serial port gets one telegram, and one telgram exactly)
+    sync = False
+
+    LOGGER.info("p1 process starting")
+
+    # Open the serial port
+    portconf = DSMR_PARAMETERS[config["dsmr"]]
+    ser = serial.Serial(
+        config["device"],
+        baudrate=portconf["speed"],
+        bytesize=portconf["databits"],
+        parity=portconf["parity"],
+        stopbits=portconf["stopbits"],
+    )
+
+    parser = P1Parser()
+    # In an endless loop, read data from the port
+    while True:
+        LOGGER.debug(
+            "Reading from serial port, sync=%s, telegram_size=%d, serial_read_size=%d",
+            sync,
+            telegram_size,
+            serial_read_size,
+        )
+        data = ser.read(max(64, serial_read_size))
+
+        # Feed data to the parser, receiving telegrams
+        telegrams = parser.feed(data)
+
+        if len(telegrams) == 0:
+            # No telegrams received. Adjust the read size down,
+            # and retry
+            LOGGER.info("Telegram length increased (was %d), sync lost", telegram_size)
+            serial_read_size = 0
+            sync = False
+            continue
+
+        # We have at least one telegram. Adjust the telegram size
+        LOGGER.debug("Received %d telegrams", len(telegrams))
+
+        new_telegram_size = len(telegrams[-1])
+
+        if new_telegram_size != telegram_size:
+            LOGGER.info(
+                "Telegram size changed %d -> %d, sync lost",
+                telegram_size,
+                new_telegram_size,
+            )
+            telegram_size = new_telegram_size
+            sync = False
+
+        # If there is data remaining in the parser, adjust our read size
+        if not sync and len(parser) == 0:
+            LOGGER.info("Sync reestablished, telegram size %d", telegram_size)
+            sync = True
+
+        serial_read_size = telegram_size - len(parser)
+
+        # Split each telegram into per-channel telegrams, and send
+        # those to the MQTT process
+        for telegram in telegrams:
+            for subtelegram in telegram.split_by_channel():
+                queue.put(subtelegram.to_mqtt())
